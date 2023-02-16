5fcb89fd42fd046d70010b52b67c1706c25e799d
Simon Walker <s.r.walker101@googlemail.com>
2023-02-16 13:10:20 +0000

    Commit

--- flake.nix
+++ flake.nix
@@ -1,19 +1,42 @@
 {
   description = "Flake utils demo";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let
+        overlays = [
+          rust-overlay.overlays.default
+        ];
+
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
+      in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.cargo
-            pkgs.rustc
-            pkgs.rustfmt
-            pkgs.clippy
-          ];
+        devShells = rec {
+          default = rust-dev;
+          rust-dev = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rust-bin.beta.latest.default
+              rust-analyzer
+              clippy
+              rustfmt
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              libiconv
+            ];
+
+            shellHook = ":";
+
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          };
 
         };
       }
